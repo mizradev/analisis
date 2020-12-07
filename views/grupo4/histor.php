c2d4c54a4e2987f73386344e7bffff6e37e369c8
Jeral <jeral.cris@gmail.com>
2020-12-06 22:11:13 -0600

    Agregando el archivo histor.php de la pantalla de historicos

---
+++ views/grupo4/histor.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="Grupo #4 Analisis y Diseño" />
+ 
+  <title>Historicos</title>
+  <!-- Custom CSS -->
+  <link href="../../dist/css/style.min.css" rel="stylesheet" />
+  <!-- Iconos -->
+  <script src="https://kit.fontawesome.com/dbe875cbad.js" crossorigin="anonymous"></script>
+</head> -->
+
+</head>
+
+<body>
+
+ <!-- ============================================================== -->
+  <!-- Preloader - style you can find in spinners.css -->
+  <!-- ============================================================== -->
+  <div class="preloader">
+    <div class="lds-ripple">
+      <div class="lds-pos"></div>
+      <div class="lds-pos"></div>
+    </div>
+  </div>
+  <!-- ============================================================== -->
+  <!-- Main wrapper - style you can find in pages.scss -->
+  <!-- ============================================================== -->
+  <div id="main-wrapper">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <?php
+    include "partes/menu-superior.php";
+    ?>
+    <!-- ============================================================== -->
+    <!-- End Topbar header -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <?php
+    include "partes/menu-izquierdo.php";
+    ?>
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper  -->
+    <!-- ============================================================== -->
+    <div class="page-wrapper">
+      <!-- ============================================================== -->
+      <!-- Bread crumb and right sidebar toggle -->
+      <!-- ============================================================== -->
+      <div class="row page-titles">
+        <div class="col-md-5 col-12 align-self-center">
+          <h3 class="text-themecolor mb-0">Historicos</h3>
+          <ul class="breadcrumb mb-0 p-0 bg-transparent">
+            <li class="breadcrumb-item active">Informacion de Clases</li>
+          </ul>
+        </div>
+      </div>
+      <!-- ============================================================== -->
+      <!-- End Bread crumb and right sidebar toggle -->
+      <!-- ============================================================== -->
+      <!-- ============================================================== -->
+      <!-- Container fluid  -->
+      <!-- ============================================================== -->
+      <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- Start Page Content -->
+        <!-- ============================================================== -->
+        <div class="widget-content searchable-container list">
+    <div class="container-fluid">
+
+
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="breadcrumb-holder">
+                    <h1 class="main-title float-left">Historicos</h1>
+                    <ol class="breadcrumb float-right">
+                        
+                    </ol>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+        <!-- end row -->
+ 
+                   <!-- Tabla -->
+          <div class="card card-body">
+            <div class="table-responsive">
+              <h5 class="">Tabla de Historicos</h5>
+              <table class="scroll-sidebar table table-striped search-table v-middle">
+                <thead class="header-item  bg-info text-white">
+                  <th class="text-center font-weight-bold">Id</th>
+                  <th class="font-weight-bold">Nombre</th>
+                  <th class="text-center font-weight-bold">Asignatura</th>
+                  <th class="text-center font-weight-bold">Ticket</th>  
+                  <th class="text-center font-weight-bold">Fecha</th>                               
+                </thead>
+
+
+        <tbody>
+                <!-- ID -->
+            <tr>
+                <td class="text-center">
+                          <span class="">
+                            
+                          </span>
+                        </td>
+                <!-- Nombre -->
+                <td>
+                          <div class="d-flex align-items-center">
+                            
+                            <div class="ml-2">
+                              <div class="user-meta-info">
+                                <h5 class="user-name mb-0" data-name="Penelope Baker">
+                                  
+                                </h5>
+                                <span class="user-work text-muted" data-occupation="Web Developer">Docente</span>
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                <!-- Facultad -->
+                <td class="text-center">
+                          <span class="">
+                            
+                          </span>
+                        </td>
+                <!-- Departamento -->
+                <td class="text-center">
+                          <span class="">
+                            
+                          </span>
+                        </td>
+                        <td class="text-center">
+                          <div class="action-btn">                         
+                          
+                          </div>
+                        </td>
+            </tr>
+
+                <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="btn-group mr-2" role="group" aria-label="First group">
+
+
+    </div>
+    <a href="https://www.unah.edu.hn/">Universidad Nacional Autónoma de Honduras. <i class=""></i></a></span>
+    
+
+
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap tether Core JavaScript -->
+  <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- apps -->
+  <script src="../../dist/js/app.min.js"></script>
+  <script src="../../dist/js/app.init.js"></script>
+  <script src="../../dist/js/app-style-switcher.js"></script>
+  <!-- slimscrollbar scrollbar JavaScript -->
+  <script src="../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+  <!--Wave Effects -->
+  <script src="../../dist/js/waves.js"></script>
+  <!--Menu sidebar -->
+  <script src="../../dist/js/sidebarmenu.js"></script>
+  <!--Custom JavaScript -->
+  <script src="../../dist/js/custom.min.js"></script>
+
+  <script>
+
+</body>
+
+</html>
